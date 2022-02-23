15d480f3da514e9f818eb085ca7b7fe3b6f7f15f
PrimtalPrimdahl <s204471@student.dtu.dk>
2022-02-23 14:52:39 +0100

    Started implementing methods for state.go

--- Database/block.go
+++ Database/block.go
@@ -1,7 +1,19 @@
 package database
 
 type Block struct {
-	ParentHash, Hash    string
-	CreatedAt, SerialNo int
-	Transactions        []Transaction
+	ParentHash, Hash string
+	CreatedAt        int64
+	SerialNo         int
+	Transactions     []Transaction
+}
+
+func createBlock(parentHash string, createdAt int64, tx []Transaction) Block {
+	block := Block{
+		parentHash,
+		"implement me",
+		makeTimestamp(),
+		69,
+		tx,
+	}
+	return block
 }
