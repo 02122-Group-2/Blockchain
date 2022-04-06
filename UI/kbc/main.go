7009a046d357f02fa1eb4093fc78cb1a82a6e77c
Aschelde <asger.foulum@gmail.com>
2022-04-06 14:26:54 +0200

    something

---
+++ UI/kbc/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+
+	var kbcCmd = &cobra.Command{
+		Use:   "kbc",
+		Short: "The Kaelder Bar Coin CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	kbcCmd.AddCommand(versionCmd)
+	kbcCmd.AddCommand(transactionCmd())
+	kbcCmd.AddCommand(balancesCmd())
+	kbcCmd.AddCommand(blockCmd())
+
+	err := kbcCmd.Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+const Major = "0"
+const Minor = "1"
+const Fix = "0"
+const Verbal = "TX Add && Balances List"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Describes version.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Version: %s.%s.%s-beta %s.", Major, Minor, Fix, Verbal)
+	},
+}
---
+++ UI/kbc/transactionCmd.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	Database "blockchain/database"
+
+	"github.com/spf13/cobra"
+)
+
+const flagFrom = "from"
+const flagTo = "to"
+const flagAmount = "amount"
+const flagType = "type"
+
+var isCreated = false
+
+func transactionCmd() *cobra.Command {
+	var transactionCmd = &cobra.Command{
+		Use:   "transaction",
+		Short: "Transaction commands (create, load)",
+		Run: func(cmd *cobra.Command, args []string) {
+		},
+	}
+
+	transactionCmd.AddCommand(transactionCreateCmd())
+
+	return transactionCmd
+
+}
+
+func transactionCreateCmd() *cobra.Command {
+	var cmd = &cobra.Command{
+		Use:   "create",
+		Short: "Create transaction to database",
+		Run: func(cmd *cobra.Command, args []string) {
+			//Initialize the flags
+			from, _ := cmd.Flags().GetString(flagFrom)
+			to, _ := cmd.Flags().GetString(flagTo)
+			amount, _ := cmd.Flags().GetUint(flagAmount)
+			typeT, _ := cmd.Flags().GetString(flagType)
+
+			//get the current state
+			state := Database.LoadState()
+			var transaction Database.Transaction
+
+			//Determine type of transaction
+			switch typeT {
+			case "genesis":
+				transaction = state.CreateGenesisTransaction(Database.AccountAddress(from), float64(amount))
+
+				fmt.Println("Genesis created" + Database.TxToString(transaction))
+
+				isCreated = true
+
+			case "reward":
+				transaction = state.CreateReward(Database.AccountAddress(from), float64(amount))
+
+				fmt.Println("Reward created" + Database.TxToString(transaction))
+
+				isCreated = true
+
+			case "transaction":
+				if to != "" {
+					transaction = state.CreateTransaction(Database.AccountAddress(from), Database.AccountAddress(to), float64(amount))
+
+					fmt.Println("Transaction created" + Database.TxToString(transaction))
+					isCreated = true
+				}
+			}
+
+			if isCreated {
+				//Add the transaction to the state and save the transactions
+				err := state.AddTransaction(transaction)
+				fmt.Println("Transaction succsesfully added")
+
+				Database.SaveTransaction(state.TxMempool)
+
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+					os.Exit(1)
+				}
+				fmt.Println("TX successfully saved")
+
+			}
+
+		},
+	}
+	cmd.Flags().String(flagFrom, "", "From what account to send tokens")
+	cmd.MarkFlagRequired(flagFrom)
+
+	cmd.Flags().String(flagTo, "", "To what account to send tokens")
+
+	cmd.Flags().Uint(flagAmount, 0, "How many tokens to send")
+	cmd.MarkFlagRequired(flagAmount)
+
+	cmd.Flags().String(flagType, "", "What type of transaction")
+	cmd.MarkFlagRequired(flagType)
+
+	return cmd
+
+}
