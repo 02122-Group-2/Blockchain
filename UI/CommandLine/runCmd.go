354799695b0349f8b566a6d82eed3dc02e2653f3
Magnus Meyer <magnus.fjellerup@gmail.com>
2022-04-06 15:39:55 +0200

    Small node change

--- UI/CommandLine/runCmd.go
+++ UI/CommandLine/runCmd.go
@@ -14,11 +14,11 @@ func runCmd() *cobra.Command {
 		Use:   "run",
 		Short: "Launches the node and HTTP API",
 		Run: func(cmd *cobra.Command, args []string) {
-			dataDir, _ := cmd.Flags().GetString(flagDataDir)
+			// dataDir, _ := cmd.Flags().GetString(flagDataDir)
 
 			fmt.Println("Starting up...")
 
-			err := Node.Run(dataDir)
+			err := Node.Run()
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
