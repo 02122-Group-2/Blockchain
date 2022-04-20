6e4bbd0361bf463c7abcceac5ba9f510818019f6
Aschelde <asger.foulum@gmail.com>
2022-04-20 15:51:32 +0200

    created code to get state and peerlist from peers

--- Node/node.go
+++ Node/node.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 type NodeState struct {
@@ -42,7 +43,15 @@ func Run() error {
 	nodeState := getNodeState()
 
 	for true {
-		for _, peer := range nodeState.peerList {
+
+		updateNodeState(&nodeState) // Updates nodeState to get local changes in case any has been made
+
+		// Creates a copy of the peers to check, to avoid weird looping as we append to the slice in the loop
+		peersToCheck := make([]string, len(nodeState.peerList))
+		copy(peersToCheck, nodeState.peerList)
+
+		for _, peer := range peersToCheck {
+
 			peerState := getPeerState(peer)
 
 			if peerState.state.LastBlockSerialNo > nodeState.state.LastBlockSerialNo {
@@ -52,10 +61,29 @@ func Run() error {
 
 			nodeState.state.TryAddTransactions(peerState.state.TxMempool)
 
+			for _, peer2 := range peerState.peerList {
+				if !contains(nodeState.peerList, peer2) && ping(peer2) {
+					nodeState.peerList = append(nodeState.peerList, peer2)
+				}
+			}
 		}
+		time.Sleep(40 * time.Second)
 	}
 
 	return nil
+}
+
+func ping(peerAddr string) bool {
+	return true
+}
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func getPeerBlocks(peerAddr string) []Database.Block {
@@ -72,6 +100,11 @@ func getNodeState() NodeState {
 	nodeState.state = *Database.LoadState()
 	nodeState.peerList = []string{bootstrapNode}
 	return nodeState
+}
+
+// Updated the current nodeState.State field to get latest local changes, in case the user has added local transactions.
+func updateNodeState(currentNodeState *NodeState) {
+	currentNodeState.state = *Database.LoadState()
 }
 
 func startNode() error {
