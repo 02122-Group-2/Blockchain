ef8829debea5f5747a58909c75a3af60ccbf388a
PrimtalPrimdahl <s204471@student.dtu.dk>
2022-06-06 14:17:40 +0200

    stopped tracking constants.go

--- Shared/constants.go
+++
@@ -1,4 +0,0 @@
-package shared
-
-const LocalDirToWallets string = "C:\\Users\\magnu\\Documents\\DTU\\FagProjekt\\Blockchain\\Blockchain\\Cryptography\\Wallets\\"
-const LocalDirToFileFolder string = "C:\\Users\\magnu\\Documents\\DTU\\FagProjekt\\Blockchain\\Blockchain\\Database\\Persistence\\"
