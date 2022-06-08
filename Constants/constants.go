cbe6fd148a2a0a09bb6d6f9dc7f1cf497376264b
Magnus Meyer <magnus.fjellerup@gmail.com>
2022-06-08 12:22:38 +0200

    Removed Constans folder and Wallets folder from commit

--- Constants/constants.go
+++
@@ -1,4 +0,0 @@
-package constants
-
-const LocalDirToWallets = "C:\\Users\\magnu\\Documents\\DTU\\FagProjekt\\Blockchain\\Blockchain\\Cryptography\\wallet"
-const LocalDirToFileFolder = "C:\\Users\\magnu\\Documents\\DTU\\FagProjekt\\Blockchain\\Blockchain\\database\\Persistence\\"
