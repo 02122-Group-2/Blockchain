480aa8e9c9dfef2d283ad2ec3475e83b2d3b3b07
Niels Raunkjær Holm <nielsraunkjaer@gmail.com>
2022-06-10 13:54:45 +0200

    Delete PeerList.json and add clause to replace PeerSet.json in the reset persistence files method

--- Shared/globalconstants.go
+++ Shared/globalconstants.go
@@ -13,6 +13,7 @@ var persistenceFileMappings []struct {
 }{
 	{from: "Blockchain_for_testing.db", to: "Blockchain.db"},
 	{from: "CurrentState_for_testing.json", to: "CurrentState.json"},
-	{from: "LatestSnapshot_for_testing.json", to: "LatestSnapshot.json"}}
+	{from: "LatestSnapshot_for_testing.json", to: "LatestSnapshot.json"},
+	{from: "PeerSet_for_testing.json", to: "PeerSet.json"}}
 
 var runtimeFiles = []string{"CurrentState.json", "LatestSnapshot.json", "state.json", "Transactions.json", "Blockchain.db", "PeerList.json", "PeerSet.json"}
